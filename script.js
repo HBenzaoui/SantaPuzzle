1952260a37f376e9d7de25eee835e963aad96334
Hamza Benzaoui <hamzapdd@gmail.com>
2018-11-03 18:23:52 +0100

    add another ways

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
 const fs = require('fs');
 
-//     console.time('santaFunc');
-//     console.timeEnd('santaFunc');
+//1- What floor does santa end up on
 
-let readable = fs.createReadStream("./input.txt", {
-    encoding: 'utf8',
-    fd: null,
-});
-readable.on('readable', function() {
-    // console.time('santaFunc');
-    let floor = 0;
-    let chunk;
-    while (null !== (chunk = readable.read(1))) {
-        if(chunk === '('){
-            // console.log('Up', chunk);
-            floor = floor + 1; 
-        } else if(chunk === ')'){
-            floor = floor - 1; 
-            // console.log('Down', chunk);
-        }
-    }
-    console.log(floor.toString())
-    // console.timeEnd('santaFunc');
-});
+//using array reduce function 
+function firstQuestion(){
+    fs.readFile('./input.txt', (err, data) =>{
+        console.time('santaFunc');
+        const directions = data.toString();
+        const directionsArray = directions.split('');
+        const answer = directionsArray.reduce((acc, currentValue) =>{
+            if(currentValue === '('){
+                return acc += 1;
+            } else if(currentValue === ')'){
+                return acc -= 1;
+            }
+        }, 0)
+        console.timeEnd('santaFunc');
+        console.log('floor:', answer);
+
+    })
+}
+
+firstQuestion();
+
+//------Using old fashion checkig--------///
+// input.on('readable', function() {
+//     // console.time('santaFunc');
+//     let floor = 0;
+//     let character;
+//     while (null !== (character = input.read(1))) {
+//         if(character === '('){
+//             // console.log('Up', character);
+//             floor = floor + 1; 
+//         } else if(character === ')'){
+//             floor = floor - 1; 
+//             // console.log('Down', character);
+//         }
+//     }
+//     console.log(floor.toString())
+//     // console.timeEnd('santaFunc');
+// });
+
+
+//2- When does Santa first enter the basement
+
+function secondQuestion(){
+    fs.readFile('./input.txt', (err, data) =>{
+        console.time('santaFunc');
+        const directions = data.toString();
+        const directionsArray = directions.split('');
+        let accumulator = 0;
+        let counter = 0;
+        const answer = directionsArray.some((currentItem) =>{
+            if(currentItem === '('){
+                accumulator += 1;
+            } else if(currentItem === ')'){
+                accumulator -= 1;
+            }
+            counter ++
+            return accumulator < 0;
+        })
+        console.timeEnd('santaFunc');
+        console.log('basment entered at:', counter);
+
+    })
+}
+
+secondQuestion();
